7eb7330901600e1ddc316a59edf16ccabf2d7d1b
unknown <richardallen0901@gmail.com>
2025-02-09 20:55:57 +0800

    first commit

---
+++ New folder/Liman-Basic-and-Fundamentals.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+const int SIZE = 10;
+int arr1[SIZE], arr2[SIZE], merged[SIZE * 2];
+int totalSize = SIZE * 2;
+cout << "Enter 10 elements for the first array:\n";
+for (int i = 0; i < SIZE; i++) {
+cin >> arr1[i];
+}
+cout << "Enter 10 elements for the second array:\n";
+for (int i = 0; i < SIZE; i++) {
+cin >> arr2[i];
+}
+for (int i = 0; i < SIZE; i++) {
+merged[i] = arr1[i];
+merged[i + SIZE] = arr2[i];
+}
+for (int i = 0; i < totalSize; i++) {
+for (int j = i + 1; j < totalSize; j++) {
+if (merged[i] < merged[j]) {
+int temp = merged[i];
+merged[i] = merged[j];
+merged[j] = temp;
+}
+}
+}
+cout << "\nMerged array in descending order:\n";
+for (int i = 0; i < totalSize; i++) {
+cout << merged[i] << " ";
+}
+return 0;
+}
